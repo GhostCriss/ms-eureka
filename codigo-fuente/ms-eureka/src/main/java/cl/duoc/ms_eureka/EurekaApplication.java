81fa738fc39137cdadc2ee25f7a22be00573e4de
Criss <168870105+GhostCriss@users.noreply.github.com>
2026-07-08 19:19:38 -0400

    Update EurekaApplication.java

--- codigo-fuente/ms-eureka/src/main/java/cl/duoc/ms_eureka/EurekaApplication.java
+++ codigo-fuente/ms-eureka/src/main/java/cl/duoc/ms_eureka/EurekaApplication.java
@@ -2,10 +2,10 @@ package cl.duoc.ms_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // <-- ASEGÚRATE DE QUE ESTE IMPORT ESTÉ ESCRITO ASÍ
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; 
 
 @SpringBootApplication
-@EnableEurekaServer // <-- Aquí ya no debería marcar error si el import y la dependencia están OK
+@EnableEurekaServer 
 public class EurekaApplication {
 
     public static void main(String[] args) {
